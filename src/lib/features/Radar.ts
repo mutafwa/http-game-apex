bcd7519d07f4d87e20326bef9f8c77662bc7f1c2
jahed323 <jrahman@my365.bellevue.edu>
2022-12-19 15:24:04 -0500

    radar 400

--- src/lib/features/Radar.ts
+++ src/lib/features/Radar.ts
@@ -8,7 +8,7 @@ export class Radar {
 
   constructor(
     private readonly canvas: HTMLCanvasElement,
-    private readonly maximumDistance = 500,
+    private readonly maximumDistance = 400,
     private readonly numberOfRings = 8) {
     this.context = canvas.getContext('2d')!;
   }
